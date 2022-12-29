ddf4cf324e1adb0efd4f82bd614f6b1637ce895c
David <david.levin2@live.biu.ac.il>
2022-12-29 20:33:19 +0200

    Parallelize clustering service

--- ClusteringService/Clustering.py
+++ ClusteringService/Clustering.py
@@ -1,32 +1,40 @@
+import time
 import pandas as pd
 from array import array
 import concurrent.futures
-from Silhouette import *
 from sklearn_extra.cluster import KMedoids
 from sklearn.metrics import silhouette_score
+from ClusteringService.Silhouette import heuristic_silhouette_value, simplified_silhouette
 
 
 class ClusteringService:
+    def __init__(self, log_service):
+        self.log_service = log_service
+
     @staticmethod
-    def execute_clustering_service(self, F: pd.DataFrame, n_features: int):
+    def execute_clustering_service(F: pd.DataFrame, n_features: int):
+        start = time.time()
         K_values = [*range(2, n_features, 1)]
 
         results = []
-        with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
             # Submit a task for each K value
-            tasks = [executor.submit(self.execute_clustering, F, K) for K in K_values]
+            tasks = [executor.submit(ClusteringService.execute_clustering, F, K) for K in K_values]
 
             # Wait for the tasks to complete and store the results
             for task in concurrent.futures.as_completed(tasks):
                 results.append(task.result())
+        end = time.time()
+        return results
 
     @staticmethod
-    def execute_clustering(self, F: pd.DataFrame, K: int) -> dict:
+    def execute_clustering(F: pd.DataFrame, K: int) -> dict:
         results = {}
 
         results['k'] = K
-        results['kmedoids'] = self.run_kmedoids(F, K)
-        results['silhouette'] = self.run_calculate_silhouette_valuekmedoids(F, results['kmedoids']['labels'])
+        results['kmedoids'] = ClusteringService.run_kmedoids(F, K)
+        results['silhouette'] = ClusteringService.calculate_silhouette_value(F, results['kmedoids']['labels'],
+                                                                             results['kmedoids']['centroids'])
         # results['single_centroid'] = self.is_new_single_centroid(results['kmedoids']['labels'])
         return results
 
@@ -52,7 +60,7 @@ class ClusteringService:
         Dictionary
             Cluster labels, Cluster centroids
         """
-        kmedoids = KMedoids(n_clusters=K, method=method, init=init, random_state=random_state).fit_predict(F)
+        kmedoids = KMedoids(n_clusters=K, method=method, init=init, random_state=random_state).fit(F)
         results = {
             'labels': kmedoids.labels_,
             'centroids': kmedoids.cluster_centers_
