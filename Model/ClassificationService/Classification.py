5fb656275325b059bb0120c7d2849e2c52cfc5d8
David <david.levin2@live.biu.ac.il>
2023-03-01 12:19:02 +0200

    Add support to new metrics

--- Model/ClassificationService/Classification.py
+++ Model/ClassificationService/Classification.py
@@ -5,8 +5,9 @@ import concurrent.futures
 from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import KFold, cross_val_score
+from sklearn.model_selection import KFold, cross_val_predict
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 
 NUMBER_OF_TEST_EPOCHS = 1
 NUMBER_OF_TRAIN_EPOCHS = 10
@@ -94,45 +95,62 @@ class ClassificationService:
 
     def evaluate(self, X: pd.DataFrame, y: pd.DataFrame, K: int, mode: str) -> dict:
         classifiers_str = []
-        classifier_to_std, classifier_to_mean = {}, {}
+        classifier_to_accuracy = {}
+        classifier_to_recall = {}
+        classifier_to_precision = {}
+        classifier_to_f1 = {}
+        classifier_to_specificity = {}
 
         for classifier in self.classifiers:
-            std_score, mean_score = 0, 0
+            accuracy_list, recall_list, precision_list, f1_list, specificity_list = [], [], [], [], []
             classifier_str = str(classifier).replace('(', '').replace(')', '')
             classifiers_str.append(classifier_str)
 
             epochs = NUMBER_OF_TRAIN_EPOCHS if mode == 'Train' else NUMBER_OF_TEST_EPOCHS
             for _ in range(epochs):
-                scores = cross_val_score(classifier, X, np.ravel(y), cv=self.cv, scoring='accuracy')
-                std_score += np.std(scores)
-                mean_score += np.mean(scores)
+                cv_predictions = cross_val_predict(classifier, X, np.ravel(y), cv=self.cv)
+                accuracy = accuracy_score(y, cv_predictions)
+                f1 = f1_score(y, cv_predictions, average='weighted')
+                recall = recall_score(y, cv_predictions, average='weighted')
+                precision = precision_score(y, cv_predictions, average='weighted')
 
-            std_score /= epochs
-            mean_score /= epochs
+                conf_mat = confusion_matrix(y, cv_predictions)
+                specificity = np.mean([conf_mat[i, i] / np.sum(conf_mat[i, :]) for i in range(conf_mat.shape[0])])
 
-            classifier_to_std[classifier_str] = std_score
-            classifier_to_mean[classifier_str] = mean_score
+                f1_list.append(f1)
+                recall_list.append(recall)
+                accuracy_list.append(accuracy)
+                precision_list.append(precision)
+                specificity_list.append(specificity)
+
+            classifier_to_f1[classifier_str] = np.mean(f1_list)
+            classifier_to_recall[classifier_str] = np.mean(recall_list)
+            classifier_to_accuracy[classifier_str] = np.mean(accuracy_list)
+            classifier_to_precision[classifier_str] = np.mean(precision_list)
+            classifier_to_specificity[classifier_str] = np.mean(specificity_list)
 
         return {
             'K': K,
             'Mode': mode,
-            'Std': classifier_to_std,
-            'Mean': classifier_to_mean,
-            'Classifiers': classifiers_str
+            'Classifiers': classifiers_str,
+            'F1': classifier_to_f1,
+            'Recall': classifier_to_recall,
+            'Accuracy': classifier_to_accuracy,
+            'Precision': classifier_to_precision,
+            'Specificity': classifier_to_specificity
         }
 
     def arrange_results(self, results: list) -> dict:
         b_results = results
-        for result in sorted(results, key=lambda x: x['K']):
-            k = result['K']
-            for classifier, classifier_res in result['Mean'].items():
-                self.log_service.log('Info', f'[Classification Service] : Accuracy result for ({k}) features with '
-                                             f'({classifier}) --> ({round(classifier_res, 3)})')
 
         new_results = {
            'Results By K': sorted(results, key=lambda x: x['K']),
-           'Results By Accuracy': sorted(results, key=lambda x: sum(x['Mean'].values()) / len(x['Mean']), reverse=True),
-           'Results By Classifiers': self.arrange_results_by_classifier(b_results)
+           'Results By Classifiers': self.arrange_results_by_classifier(b_results),
+           'Results By F1': sorted(results, key=lambda x: sum(x['F1'].values()) / len(x['F1']), reverse=True),
+           'Results By Recall': sorted(results, key=lambda x: sum(x['Recall'].values()) / len(x['Recall']), reverse=True),
+           'Results By Accuracy': sorted(results, key=lambda x: sum(x['Accuracy'].values()) / len(x['Accuracy']), reverse=True),
+           'Results By Precision': sorted(results, key=lambda x: sum(x['Precision'].values()) / len(x['Precision']), reverse=True),
+           'Results By Specificity': sorted(results, key=lambda x: sum(x['Specificity'].values()) / len(x['Specificity']), reverse=True),
         }
         return new_results
 
@@ -145,7 +163,7 @@ class ClassificationService:
             classifiers_res = {}
             for result in results:
                 K = result['K']
-                classification_results = result['Mean']
+                classification_results = result['Accuracy']
                 for c, res in classification_results.items():
                     if c == classifier:
                         classifiers_res[K] = res
--- Model/TableService/Table.py
+++ Model/TableService/Table.py
@@ -8,48 +8,51 @@ class TableService:
     def create_table(fold_index: str, stage: str, classification_res: dict):
         train_results = test_results = None
 
-        if 'Train' in classification_res:
-            train_results = classification_res['Train']['Results By K']
-            headers = ['K Value'] + classification_res['Train']['Results By K'][0]['Classifiers']
-        if 'Test' in classification_res:
-            test_results = classification_res['Test']['Results By K']
-            headers = ['K Value'] + classification_res['Test']['Results By K'][0]['Classifiers']
+        metrics = ['F1', 'Recall', 'Accuracy', 'Precision', 'Specificity']
 
-        seperator = ['*'] * len(headers)
-        table = PrettyTable([header for header in headers])
+        for metric in metrics:
+            if 'Train' in classification_res:
+                train_results = classification_res['Train']['Results By K']
+                headers = ['K Value'] + classification_res['Train']['Results By K'][0]['Classifiers']
+            if 'Test' in classification_res:
+                test_results = classification_res['Test']['Results By K']
+                headers = ['K Value'] + classification_res['Test']['Results By K'][0]['Classifiers']
 
-        if train_results is not None:
-            for train_result in train_results:
-                row = [train_result['K']] + list(train_result['Mean'].values())
-                table.add_row([col for col in row])
+            seperator = ['*'] * len(headers)
+            table = PrettyTable([header for header in headers])
 
-        if train_results is not None and test_results is not None:
-            table.add_row([sep for sep in seperator])
+            if train_results is not None:
+                for train_result in train_results:
+                    row = [train_result['K']] + list(train_result[metric].values())
+                    table.add_row([col for col in row])
 
-        if test_results is not None:
-            for test_result in test_results:
-                row = [test_result['K']] + list(test_result['Mean'].values())
-                table.add_row([col for col in row])
+            if train_results is not None and test_results is not None:
+                table.add_row([sep for sep in seperator])
 
-        TableService.save_table(fold_index, stage, table)
+            if test_results is not None:
+                for test_result in test_results:
+                    row = [test_result['K']] + list(test_result[metric].values())
+                    table.add_row([col for col in row])
+
+            TableService.save_table(table, metric, fold_index, stage)
 
     @staticmethod
-    def save_table(fold_index, stage, table):
+    def save_table(table: PrettyTable, metric: str, fold_index: str, stage: str):
         try:
             current_dir = os.path.dirname(__file__)
             plots_dir = os.path.join(current_dir, '..', 'Files', 'Plots')
             latest_plot_dir = max(glob.glob(os.path.join(plots_dir, '*/')), key=os.path.getmtime).rsplit('\\', 1)[0]
 
             if stage == "Train":
-                table_file = os.path.join(latest_plot_dir, stage, f'Fold #{fold_index}', 'Results.txt')
+                table_file = os.path.join(latest_plot_dir, stage, f'Fold #{fold_index}', f'{metric} Results.txt')
             else:
                 dir = os.path.join(latest_plot_dir, stage)
                 if not os.path.isdir(dir):
                     os.makedirs(dir)
-                table_file = os.path.join(latest_plot_dir, stage, 'Results.txt')
+                table_file = os.path.join(latest_plot_dir, stage, f'{metric} Results.txt')
 
             with open(table_file, 'w') as w:
-                data = table.get_string(title="Classification Results")
+                data = table.get_string(title=f"Classification Results - {metric}")
                 w.write(data)
             w.close()
         except OSError as ex:
